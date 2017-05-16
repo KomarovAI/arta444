740a4f70bb765f444df65809b9e4c1dd6c7d3637
PC <ar@we.ru>
2017-05-16 21:22:26 +0300

    228

--- app/src/main/java/com/example/pc/karta4/Server.java
+++ app/src/main/java/com/example/pc/karta4/Server.java
@@ -71,6 +71,7 @@ public class Server extends Activity{
         b3 = (ImageView) findViewById(R.id.imageView3);
 
         mt = new ClientServer();
+        new Thread(new Strngs()).start();
                 mt.execute();
 
 //        new Thread(new ClientServer()).start();
@@ -122,116 +123,118 @@ public class Server extends Activity{
         alertDialog.show();
     }
 
+public class Strngs extends Thread {
+    public void run() {
+        try {
+            servers = new ServerSocket(4444);
+        } catch (IOException e1) {
+            System.out.println("Couldn't listen to port 4444");
+            System.exit(-1);
+        }
+        while (true) {
+            try {
+                //    System.out.print("Waiting for a client...");
+                fromclient = servers.accept();
+                System.out.println("Client connected");
+            } catch (IOException e) {
+                System.out.println("Can't accept");
+                System.exit(-1);
+            }
+
+            try {
+                in = new BufferedReader(new
+                        InputStreamReader(fromclient.getInputStream()));
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            try {
+                input = in.readLine();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            if (count != 3) {
+                if (pas1.equals(input.substring(17))) {
+                    System.out.println("Пароль верен!");
+                    mac1 = input.substring(0, 17);
+                    count++;
+                } else if (pas2.equals(input.substring(17))) {
+                    System.out.println("Пароль верен!");
+                    mac2 = input.substring(0, 17);
+                    count++;
+                } else if (pas3.equals(input.substring(17))) {
+                    System.out.println("Пароль верен!");
+                    mac3 = input.substring(0, 17);
+                    count++;
+                }
+            } else {
+                String mac = input.substring(0, 17);
+                System.out.println(mac);
+                input = input.substring(17);
+                System.out.println(input);
+                if (mac.equals(mac1)) {
+                    if (red.equals(input) && !Jud1) {
+                        JudF1 = red;
+                        Jud1 = true;
+                    } else if (blue.equals(input) && !Jud1) {
+                        JudF1 = blue;
+                        Jud1 = true;
+                    } else if (yellow.equals(input) && !Jud1) {
+                        JudF1 = yellow;
+                        Jud1 = true;
+                    } else if (green.equals(input) && !Jud1) {
+                        JudF1 = green;
+                        Jud1 = true;
+                    }
+                } else if (mac.equals(mac2)) {
+                    if (red.equals(input) && !Jud2) {
+                        JudF2 = red;
+                        Jud2 = true;
+                    } else if (blue.equals(input) && !Jud2) {
+                        JudF2 = blue;
+                        Jud2 = true;
+                    } else if (yellow.equals(input) && !Jud2) {
+                        JudF2 = yellow;
+                        Jud2 = true;
+                    } else if (green.equals(input) && !Jud2) {
+                        JudF2 = green;
+                        Jud2 = true;
+                    }
+                } else if (mac.equals(mac3)) {
+                    if (red.equals(input) && !Jud3) {
+                        JudF3 = red;
+                        Jud3 = true;
+                    } else if (blue.equals(input) && !Jud3) {
+                        JudF3 = blue;
+                        Jud3 = true;
+                    } else if (yellow.equals(input) && !Jud3) {
+                        JudF3 = yellow;
+                        Jud3 = true;
+                    } else if (green.equals(input) && !Jud3) {
+                        JudF3 = green;
+                        Jud3 = true;
+                    }
+                }
+
+            }
+        }
+    }
+}
 
     public class ClientServer extends AsyncTask<Void,Void,Void> {
 
         protected void onPreExecute() {
             super.onPreExecute();
-
-            try {
-                servers = new ServerSocket(4444);
-            } catch (IOException e1) {
-                System.out.println("Couldn't listen to port 4444");
-                System.exit(-1);
-            }
-
         }
+
         @Override
         protected Void doInBackground(Void... params) {
-            while (true) {
-
-
-                try {
-                    //    System.out.print("Waiting for a client...");
-                    fromclient = servers.accept();
-                    System.out.println("Client connected");
-                } catch (IOException e) {
-                    System.out.println("Can't accept");
-                    System.exit(-1);
-                }
-
-                try {
-                    in = new BufferedReader(new
-                            InputStreamReader(fromclient.getInputStream()));
-                } catch (IOException e) {
-                    e.printStackTrace();
-                }
-                try {
-                    input = in.readLine();
-                } catch (IOException e) {
-                    e.printStackTrace();
-                }
-                if (count != 3) {
-                    if (pas1.equals(input.substring(17))) {
-                        System.out.println("Пароль верен!");
-                        mac1 = input.substring(0, 17);
-                        count++;
-                    } else if (pas2.equals(input.substring(17))) {
-                        System.out.println("Пароль верен!");
-                        mac2 = input.substring(0, 17);
-                        count++;
-                    } else if (pas3.equals(input.substring(17))) {
-                        System.out.println("Пароль верен!");
-                        mac3 = input.substring(0, 17);
-                        count++;
-                    }
-                } else {
-                    String mac = input.substring(0, 17);
-                    System.out.println(mac);
-                    input = input.substring(17);
-                    System.out.println(input);
-                    if (mac.equals(mac1)) {
-                        if (red.equals(input) && !Jud1) {
-                            JudF1 = red;
-                            Jud1 = true;
-                        } else if (blue.equals(input) && !Jud1) {
-                            JudF1 = blue;
-                            Jud1 = true;
-                        } else if (yellow.equals(input) && !Jud1) {
-                            JudF1 = yellow;
-                            Jud1 = true;
-                        } else if (green.equals(input) && !Jud1) {
-                            JudF1 = green;
-                            Jud1 = true;
-                        }
-                    } else if (mac.equals(mac2)) {
-                        if (red.equals(input) && !Jud2) {
-                            JudF2 = red;
-                            Jud2 = true;
-                        } else if (blue.equals(input) && !Jud2) {
-                            JudF2 = blue;
-                            Jud2 = true;
-                        } else if (yellow.equals(input) && !Jud2) {
-                            JudF2 = yellow;
-                            Jud2 = true;
-                        } else if (green.equals(input) && !Jud2) {
-                            JudF2 = green;
-                            Jud2 = true;
-                        }
-                    } else if (mac.equals(mac3)) {
-                        if (red.equals(input) && !Jud3) {
-                            JudF3 = red;
-                            Jud3 = true;
-                        } else if (blue.equals(input) && !Jud3) {
-                            JudF3 = blue;
-                            Jud3 = true;
-                        } else if (yellow.equals(input) && !Jud3) {
-                            JudF3 = yellow;
-                            Jud3 = true;
-                        } else if (green.equals(input) && !Jud3) {
-                            JudF3 = green;
-                            Jud3 = true;
-                        }
-                    }
-                }
-
-                return null;
-            }
+            return null;
         }
 
 
         protected void onPostExecute(Void result) {
             super.onPostExecute(result);
+            while (true) {
                 try {
                     Thread.sleep(1000);
                 } catch (InterruptedException e) {
@@ -281,9 +284,8 @@ public class Server extends Activity{
                     b2.setBackgroundColor(Color.BLACK);
                     b3.setBackgroundColor(Color.BLACK);
                 }
-                gg=false;
             }
-
+            }
         }
     }
 
